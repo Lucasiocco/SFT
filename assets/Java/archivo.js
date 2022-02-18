64609dc2c513b71b7129bcfb912402d1fff624cb
lucas_iocco <lucas.nahuel.iocco@gmail.com>
2022-02-18 00:03:39 -0300

    Se modifico la hero section y se arreglo el responsive

--- assets/Java/archivo.js
+++ assets/Java/archivo.js
@@ -151,8 +151,7 @@
         }
       }
     });
-  
-    /**
+      /**
      * Intro type effect
      */
     const typed = select('.typed')
@@ -177,5 +176,29 @@
         preloader.remove()
       });
     }
-  
+    /**
+     * Intro type effect
+     */
+      const typed = select('.typed')
+      if (typed) {
+        let typed_strings = typed.getAttribute('data-typed-items')
+        typed_strings = typed_strings.split(',')
+        new Typed('.typed', {
+          strings: typed_strings,
+          loop: true,
+          typeSpeed: 100,
+          backSpeed: 50,
+          backDelay: 2000
+        });
+      }
+
+    /**
+      * Preloader
+      */
+      let preloader = select('#preloader');
+      if (preloader) {
+        window.addEventListener('load', () => {
+          preloader.remove()
+        });
+      }
 })()
